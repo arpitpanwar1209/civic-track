9e50c97b9a7ac5f7a18510bf7b1fa4fadcdf3c97
arpitpanwar1209 <141935499+arpitpanwar1209@users.noreply.github.com>
2025-08-02 11:53:50 +0530

    changes in first working commit

--- accounts/views.py
+++ accounts/views.py
@@ -3,5 +3,13 @@ from django.shortcuts import render
 def login_view(request):
     return render(request, 'accounts/login.html')
 
-def register_view(request):
-    return render(request, 'accounts/register.html')
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('accounts:login')  # You'll add this next
+    else:
+        form = UserCreationForm()
+    return render(request, 'accounts/signup.html', {'form': form})
